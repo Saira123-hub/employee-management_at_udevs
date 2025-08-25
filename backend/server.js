484819214c624d5b0ead671d6f2eeb7aebb0cec5
SairaBatool <sb4106220@gmail.com>
2025-08-26 01:38:55 +0500

    Add full project: frontend + backend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+
+
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// Import Routes
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+// app.use(cors({ origin: "http://localhost:3000"}));
+app.use(cors({ origin: "http://localhost:5173" }));
+// app.use(cors());
+// Use Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
